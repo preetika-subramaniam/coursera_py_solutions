4406d724ee39a001f99d5b40eecfffab68f5314f
preetika18 <preetika.ee@gmail.com>
2018-10-20 21:57:06 +0530

    Completed week 6

---
+++ Capstone Project- Python/Week 4 - 7/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','indiana.edu','uct.ac.za','ufp.pt','ucdavis.edu','columbia.edu','berkeley.edu','etudes.org','gmail.com','cam.ac.uk'],
+['2005-12',57,12,14,10,11,13,12,5,10,7],
+['2006-01',86,29,25,29,26,22,22,24,15,15]
+];
